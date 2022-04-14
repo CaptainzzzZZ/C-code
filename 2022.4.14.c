9de7f5118991359dc1f8b9eacc5cc5207a864345
小醒目 <1779289666@qq.com>
2022-04-14 23:42:51 +0800

    2022.4.14.c

---
+++ 2022.4.14.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("wwwwwsadawdsd  wadwdaawdadawd");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int arr[5][3] = { { 1,2,3,}, { 3,2,1 },{1,2,3},{4,5,6},{7,8,9} };
+//	//printf("%d ", arr[4][2]);
+//	for (i=0; i < 5; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//			
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int i;
+//	int k = 3;
+//	for (i = 0; i < k; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//	scanf("%d", &i);
+//
+//	printf("%d\n", arr[k]);
+//	printf("%d", i);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[5] = { 1,2 };
+//	printf("%d\n", arr[4]);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//		for (j = i; j < 10; j += 2)
+//			printf("%d ", j);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (; ;)//无判断 恒为真 死循环
+// 
+//		printf("6666");
+//	return 0;
+//int main()
+//{
+//	int i = 0;
+//	for (i=0; i = 0; i++)
+//		printf("%d\n", i);
+//	return 0;
+//}
