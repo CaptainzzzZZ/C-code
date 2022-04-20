370a5092763b8250d5df1a922004905df32cea0a
小醒目 <1779289666@qq.com>
2022-04-20 23:33:03 +0800

    2022.4.20

---
+++ 4.20.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int temp=0;
+//	int j = 0;
+//	int arr[6] = {6626,5959,4515,30,2,1};
+//
+//	for (i = 1; i <= 5 ; i++)
+//	{
+//		for (j = 0; j <=5-i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				temp=arr[j];
+//			    arr[j]=arr[j+1];
+//				arr[j+1] = temp;
+//			}
+//		}
+//	}
+//	for (i= 0; i< 6; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//void math()
+//{
+//	printf("123456");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	math();
+//	printf("\n654321\n");
+//	return 0;
+//}
