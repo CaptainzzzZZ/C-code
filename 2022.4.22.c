b894d206ca5cba886dc8233f3ad6def847a2f9d2
小醒目 <1779289666@qq.com>
2022-04-22 23:27:59 +0800

    自定义函数

---
+++ 2022.4.22.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+
+//get_min(int x, int y)
+//{
+//	int d = 0;
+//	if (x < y)
+//		d = x;
+//	else
+//		d = y;
+//	return d;
+//}
+//
+//int main()
+//{
+//	int a = 666;
+//	int b = 6666;
+//	int c = 66666;
+//	int min = get_min(a, b);
+//	printf("min=%d\n", min);
+//
+//	return 0;
+//}
+
+//void exchange(int* pa, int* pb)
+//{
+//	int temp = 0;
+//	if (*pa!=*pb)
+//	{
+//		temp = *pa;
+//		*pa=*pb;
+//		*pb= temp;
+//		//return 0;
+//
+//	}
+//}
+//
+////pa是地址所以是址传递
+////*pa==a;
+//
+//int main()
+//{
+//
+//	int a = 66;
+//	int b = 666;
+//	int temp = 0;
+//	exchange(&a,&b);//exchange(a,b);
+//	printf("a=%d b=%d\n", a, b);
+//	return 0;
+//}
+
