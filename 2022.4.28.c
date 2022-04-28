480de6e28e8a595647636e8b862db279b6f2de3f
小醒目 <1779289666@qq.com>
2022-04-28 23:44:11 +0800

    函数递归

---
+++ 2022.4.28.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNNING
+
+#include<stdio.h>
+
+//int main()
+//{
+//	//×ªÒå×Ö·û
+//	printf("%d\n", '\x06');
+//	printf("%d\n", '\x6A');
+//	printf("%c\n", '\x06A');
+//	printf("%d\n", '\0006');//'6'=54
+//
+//	return 0;
+//}
+
+//º¯ÊýµÝ¹é
+
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	print(num);
+//	return 0;
+//}
+
+//int fact(int n)
+//{
+//	if (1 == n)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return n * fact(n - 1);
+//	}
+//}
+//
+//int main()
+//{
+//	int a = 5;
+//	int res = fact(a);
+//	printf("%d", res);
+//
+//	return 0;
+//}
