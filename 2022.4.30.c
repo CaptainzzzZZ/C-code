198e2f272d11c1c611fc6edb631ebf66c93dbb75
小醒目 <1779289666@qq.com>
2022-04-30 23:53:53 +0800

    函数递归2

---
+++ 2022.4.30.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNNING 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int i = 0;
+//	int res = 1;
+//
+//	for (i = 1; i <= n; i++)
+//	{
+//		res = res * i;
+//	}
+//	printf("%d\n", res);
+//	return 0;
+//}
+
+//º¯ÊýµÝ¹é
+
+
+//total(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * total(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int res=total(n);
+//	printf("%d\n", res);
+//	
+//	return 0;
+//}
+
+
+//int fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,1 };
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	/*int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		arr[i+2 ] = arr[i] + arr[i +1];
+//		printf("%d ", arr[i]);
+//	}*/
+//	int res=fib(n);
+//	printf("%d", res);
+//	return 0;
+//}
