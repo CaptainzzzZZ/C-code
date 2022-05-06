22a07b51ddd6bd75d2fbc57f0362b883cd02f0ba
小醒目 <1779289666@qq.com>
2022-05-07 00:09:48 +0800

    洛谷入门练习题

---
+++ 2022.5.6.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf_s("%d %d", &a, &b);
+//    printf("%d\n", a + b);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int mian()
+//{
+//    int a = 0, b = 0, c = 0;
+//    scanf_s("%d %d %d", &a, &b, &c);
+//    printf("%d", b);
+//    return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0, b = 0, c = 0;
+//    scanf_s("%8d %8d %8d", &a, &b, &c);
+//    printf("%11d %11d %11d", a, b, c);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    char c = 0;
+//    scanf_s("%c",&c);
+//    printf("  %c\n", c);
+//    printf(" %c%c%c\n",c,c,c);
+//    printf("%c%c%c%c%c\n",c,c,c,c,c);
+//    return 0;
+//}
