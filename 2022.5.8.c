4c503e6539387cb381287e915d561f2d064b78a9
小醒目 <1779289666@qq.com>
2022-05-08 18:57:44 +0800

    牛客c入门

---
+++ 2022.5.8.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int k = 0;
+//	for (i = 1; i <= 3; i++)
+//	{
+//		for (j = i; j < 3; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (k = 1; k <= 2 * i - 1; k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//void main() {
+//    int a = 4;
+//    printf("%d", ++(a++));
+//}
+
+
+//#include<stdio.h>
+//int fun(int x) {
+//    static int a = 3;
+//    x += a++;
+//    return x;
+//}
+//void main() {
+//    int x = 2, sum;
+//    sum = fun(x);
+//    sum = sum + fun(x);
+//    printf("%d\n", sum);
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    float a = 0, b = 0;
+//    scanf_s("%f", &a);
+//    scanf_s("%f", &b);
+//    printf("%.3f%%", a / b);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    double F = 0, C = 0;
+//    scanf_s("%lf", &F);
+//    printf("%lf", C = 5 * (F - 32) / 9);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//#define pi 3.14159
+//int main()
+//{
+//    float r = 0;
+//    scanf("%f", &r);
+//    printf("%.4f %.4f %.4f", 2 * r, 2 * pi * r, pi * r * r);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//
+//#define add(x) x*x
+//int main()
+//{
+//	int a = 6;
+//	int b = 3;
+//	printf("%d", add(b)*3);
+//	return 0;
+//}
