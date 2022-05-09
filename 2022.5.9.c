80375c3f65fe84c62969a764d3311d4b188c5824
小醒目 <1779289666@qq.com>
2022-05-09 23:35:38 +0800

    一维数组和洛谷入门题

---
+++ 2022.5.9.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char ch1[] = "hello";
+//	char ch2[6] = { 'h','e','l','l','o'};
+//	printf("%d ", (strlen(ch1)));
+//	printf("%d ", sizeof(ch1));
+//	printf("%d ", (strlen(ch2)));
+//	printf("%d ", sizeof(ch2));
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	printf("%d\n", 0x12);
+//	printf("%p\n", 0x12);
+//	printf("%x\n", 0x12);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//	int i = 0;
+//	int* p = arr;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    float r1, r2, R;
+//    scanf("%f %f", &r1, &r2);
+//    printf("%.2f", R = 1 / (1 / r1 + 1 / r2));
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    float x = 0;
+//    scanf("%f", &x);
+//    printf("%.0f", x);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    char a = '0';
+//    scanf("%c", &a);
+//    printf("%d", a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    scanf("%d", &a);
+//    printf("%c", a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdbool.h>
+//int main()
+//{
+//    int a = 0;
+//    bool b = 0;
+//    //int c = 0;
+//    scanf("%d", &a);
+//    b = a;
+//    //c = b;
+//    printf("%d", b);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a, b, c, d, e;
+//    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+//    printf("%d %d %d %d %d", a / 3 + e / 3 + b / 3, b / 3 + a / 3 + c / 3, c / 3 + b / 3 + d / 3, d / 3 + c / 3 + e / 3, e / 3 + d / 3 + a / 3);
+//    printf("%d", a % 3 + b % 3 + c % 3 + d % 3 + e % 3);
+//    return 0;
+//}
