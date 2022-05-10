c2f934692d67b995d3282553e0b8a0a6e7727592
小醒目 <1779289666@qq.com>
2022-05-10 23:34:13 +0800

    二维数组和洛谷入门

---
+++ 2022.5.10.c
@@ -0,0 +1,122 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr[][3] = {0,1,2,3,4,5,6,7,8,9 };
+//	int i = 0;
+//	int j = 0;
+//	int* p = &arr[0][0];
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			//printf("%d ", arr[i][j]);
+//			printf("&arr[%d][%d]=%p    ", i, j,p);
+//			p++;
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[][9] = { 0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 7; i++)
+//	{
+//		for (j = 0; j < 9; j++)
+//		{
+//			if (arr[i][j] = 0)
+//				printf(" ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//    float a = 0;
+//    scanf("%f", &a);
+//    printf("%.3f", a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    double a = 0;
+//    scanf("%lf", &a);
+//    printf("%.12lf", a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    char ch = 0;
+//    int a = 0;
+//    float b = 0;
+//    double c = 0;
+//    scanf("%c", &ch);
+//    scanf("%d", &a);
+//    scanf("%f", &b);
+//    scanf("%lf", &c);
+//    printf("%c %d %.6f %.6lf", ch, a, b, c);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    double a = 0;
+//    scanf("%lf", &a);
+//    printf("%lf", a);
+//    printf("%.5lf", a);
+//    printf("%e", a);
+//    printf("%g", a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int arr[5][5] = { 0,0,1,0,0,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,0,1,0,0 };
+//    int i = 0;
+//    int j = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        for (j = 0; j < 5; j++)
+//        {
+//            if (arr[i][j] = 0)
+//            {
+//                printf(" ");
+//            }
+//            else
+//            {
+//                printf("*");
+//            }
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+    printf("  *\n");
+    printf(" ***\n");
+    printf("*****\n");
+    printf(" ***\n");
+    printf("  *\n");
+    return 0;
+}
