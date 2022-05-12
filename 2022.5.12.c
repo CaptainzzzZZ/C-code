763d234964ebc2add144894422f8830723e7b5ed
小醒目 <1779289666@qq.com>
2022-05-12 23:53:40 +0800

    洛谷入门 大象喝水

---
+++ 2022.5.12.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//#define pi 3.14
+//int main()
+//{
+//    int h, r;
+//    scanf("%d %d", &h, &r);
+//    float v = (pi * r * r * h);
+//    float ret = 20000 / v;
+//    printf("%.0f", ret + 1);
+//    return 0;
+//}
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A * B);
+    return 0;
+}
