474649cdbd880c895feaa9a7047d19c098ef5390
小醒目 <1779289666@qq.com>
2022-05-15 00:00:12 +0800

    洛谷比赛5.14

---
+++ 2022.5.13.C
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    int T = 0;
+//    scanf("%d", &T);
+//
+//    int x = 0;
+//    int y = 0;
+//    int z = 0;
+//    scanf("%d %d %d", &x, &y, &z);
+//    if ((z - 1) >= (y / x))
+//    {
+//        printf("Renko");
+//    }
+//
+//    else if ((z - 1) >= (y / x) + z)
+//    {
+//        printf("Renko");
+//    }
+//    else
+//    {
+//        printf("Merry");
+//    }
+//    return 0;
+//}
