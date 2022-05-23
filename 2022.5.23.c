56ea5e9ac67113b75ba1ab18b9e35ba0ca2219c4
小醒目 <1779289666@qq.com>
2022-05-23 23:57:48 +0800

    算术操作符

---
+++ 2022.5.23.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = a >> 1;
+//	printf("%d", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	//b = a ^ b ^ b;
+//	//a = a & b;
+//	b = a | b;
+//	printf("%d\n", b);
+//
+//	printf("%d",a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	/*b = a;
+//	a = b;*/
+//	/*a = b ^ a^a;
+//	b = a ^ a ^ a;*/
+//	a = b ^ a;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	float   a = 6/ 5.0f;
+//	printf("%f", a);
+//	double b = 2.0 / 3;
+//	printf("\n%lf", b);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//
+//{
+//
+//    int n = 0;
+//    scanf_s("%d", &n);
+//    int i, x, y, z, sum;
+//    int a = 0, b = 0, c = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf_s("%d %d %d", &x, &y, &z);
+//        a = a + x;
+//        b = b + y;
+//        c = c + z;
+//        sum = a + b + c;
+//    }
+//    printf("%d %d %d", a, b, c);
+//    return 0;
+//}
