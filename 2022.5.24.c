7c85f4fe368e72ee3a3b4244b1e087b3e6516771
小醒目 <1779289666@qq.com>
2022-05-24 23:43:16 +0800

    上课指针，运算符，洛谷

---
+++ 2022.5.24.c
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//
+//int main()
+//{
+//	short s = 5;
+//	int a = 10;
+//	printf("%zd", sizeof(s = a + 2));
+//	printf("%d", s);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -3;
+//	int b = ~a;
+//	printf("%d", b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%d", sizeof(int[10]));
+//	printf("%d", sizeof(int[10]));
+//	printf("%d", sizeof(int[10]));
+//	printf("%d", sizeof(int[10]));
+//	printf("%d", sizeof(int[10]));
+//
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int m, n;
+//    scanf("%d %d", &m, &n);
+//    int i = 0, sum = 0;
+//    for (i = m; i <= n; i++)
+//    {
+//        if (i % 17 == 0)
+//        {
+//            sum += i;
+//        }
+//    }
+//    printf("%d", sum);
+//    return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    int k, x, count1 = 0, count5 = 0, count10 = 0;
+//    scanf("%d", &k);
+//    int i = 0;
+//    for (i = 0; i < k; i++)
+//    {
+//        scanf("%d", &x);
+//        if (x == 1)
+//            count1++;
+//        else if (x == 5)
+//            count5++;
+//        else if (x == 10)
+//            count10++;
+//    }
+//    printf("%d", count1);
+//    printf("%d", count5);
+//    printf("%d", count10);
+//
+//    return 0;
+//}
+
+//
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    double a = 0, n = 0;
+//    scanf("%lf %lf", &a, &n);
+//    printf("%lf", pow(a, n));
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    long int a = 0, n = 0, ret = 1;
+//    scanf("%ld %ld", &a, &n);
+//    for (int i = 0; i <n; i++)
+//    {
+//        ret = ret * a;
+//    }
+//    printf("%d", ret);
+//    return 0;
+//
+//}
+
+//void swap(int a, int b)
+//{
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//}
+
+//void swap(int* a, int* b)
+//{
+//	int temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//int main()
+//{
+//	int a = 3, b = 6;
+//	printf("%d %d \n", a, b);
+//	//swap(a, b);
+//	swap(&a, &b);
+//	printf("%d %d \n", a, b);
+//
+//	return 0;
+//}
