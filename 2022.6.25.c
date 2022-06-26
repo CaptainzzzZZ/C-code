6a0c4dcca226fd3e5fec1a838ae1b58d420f39a4
小醒目 <1779289666@qq.com>
2022-06-26 23:29:25 +0800

    大一假期第一天

---
+++ 2022.6.25.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	int a=5 > 6 ? 1 : 2;
+//	printf("%d", a);
+//	return 0;
+//}
+
+
+//void menu()
+//{
+//	printf("******************************\n");
+//	printf("********1.四则运算************\n");
+//	printf("********2.三角转换************\n");
+//	printf("********3.退出程序************\n");
+//	printf("******************************\n");
+//}
+//void size()
+//{
+//	int a = 0, b = 0;
+//	char c = 0;
+//	printf("请输入a和b的值和要选择的运算符\n");
+//	scanf("%d %d %c", &a, &b, &c);
+//	if ('+' == c)
+//		printf("%d\n", a + b);
+//	else if ('-' == c)
+//		printf("%d\n", a - b);
+//	else if ('*' == c)
+//		printf("%d", a * b);
+//	else if ('/' == c)
+//	{
+//		if (b == 0)
+//		{
+//			printf("被除数不能为0无意义");
+//		}
+//		else
+//		{
+//			printf("%d\n", a / b);
+//		}
+//	}
+//}
+//void trian()
+//{
+//	double pi = 3.1415926;
+//	double angle = 0;
+//	printf("请输入角度：");
+//	scanf("%lf", &angle);
+//	printf("%lf\n", sin(angle / 180 * pi));
+//	printf("%lf\n", cos(angle / 180 * pi));
+//	printf("%lf\n", tan(angle / 180 * pi));
+//	printf("%lf\n", asin(angle / 180 * pi));
+//
+//
+//}
+//int main()
+//{
+//	menu();
+//	int input = 0;
+//	printf("请选择模式 ：");
+//	scanf("%d", &input);
+//	switch (input)
+//	{
+//	case 1:size(); break;
+//	case 2:trian(); break;
+//	case 3:break;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	
+//	return 0;
+//}
