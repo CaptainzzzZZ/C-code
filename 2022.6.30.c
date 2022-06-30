a59149c8b14163a8fcc2566e90b29d7621e33984
小醒目 <1779289666@qq.com>
2022-06-30 22:05:14 +0800

    假期第二次练习

---
+++ 2022.6.30.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+
+//void init(int arr[], int x)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		arr[i] = 0;
+//		printf("%d ", arr[i]);
+//
+//	}
+//	printf("\n");
+//}
+//
+//void print(int arr[], int x)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int temp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = temp;
+//		left++;
+//		right--;
+//	}
+//	for(int i=0;i<sz;i++)
+//	printf("%d ",arr[i]);
+//
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr)/sizeof(arr[0]);
+//	print(arr, sz);
+//	reverse(arr, sz);
+//	printf("\n");
+//	init(arr, sz);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1,3,5,7,9 };
+//	int brr[] = { 2,4,6,8,10 };//
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int temp = arr[i];
+//		arr[i] = brr[i];
+//		brr[i] = temp;
+//	}
+//	return 0;
+//}
+
