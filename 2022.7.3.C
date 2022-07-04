d4cf07bf8864b73fb34775c24f3ad379715204a4
小醒目 <1779289666@qq.com>
2022-07-04 22:30:58 +0800

    洛谷入门

---
+++ 2022.7.3.C
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);	
+//	printf("  %c\n", ch);
+//	printf(" %c%c%c\n", ch,ch,ch);
+//	printf("%c%c%c%c%c\n", ch,ch,ch,ch,ch);
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d", &a);
+//	scanf("%d", &b);
+//	printf("%.3f%%", (100.0 * b / a));
+//	return 0;
+//}
+
+//int main()
+//{
+//	float a = 0;
+//	scanf("%f", &a);
+//	printf("%lld", (long long)a);
+//	return 0;
+//}
+//double pi = 3.14159;
+//int main()
+//{
+//	double r = 0;
+//	scanf("%lf", &r);
+//	printf("%.4lf %.4lf %.4lf", 2 * r, 2 * pi * r, pi * r * r);
+//
+//	return 0;
+//}
+
+//double pi = 3.14;
+//int main()
+//{
+//	//unsigned int 
+//	int r = 0;
+//	scanf("%d", &r);
+//	printf("%.5lf", (double)(4*1.0 / 3 * pi * r * r * r));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	double x = 0;
+//	double y = 0;
+//	scanf("%lf", &x);
+//	if (x >= 0 && x < 5)
+//	{
+//		y = 2.5 - x;
+//	}
+//	else if (x >= 5 && x < 10)
+//	{
+//		y = 2-(1.5*(x-3)*(x-3));
+//	}
+//	else if (x >= 10 && x < 20)
+//	{
+//		y = x / 2 - 1.5;
+//	}
+//	printf("%.3lf", y);
+//	return 0;
+//}
+
+
